e86a8d9fd3090a9507fa33ac59718691dd9380d6
Justyna Kowalewska <justynajaglowska1@gmail.com>
2023-05-08 11:36:52 +0200

    adding Tytuł

---
+++ src/components/Tytuł/index.js
@@ -0,0 +1,14 @@
+import { StylledFront, Title, Subtitle } from "./styled";
+
+const Tytuł = () => (
+    <StylledFront>
+        <Title>
+            Świadectwa Charakterystyki Energetycznej
+        </Title>
+        <Subtitle>
+            Sejny - Suwałki - Augustów
+        </Subtitle>
+    </StylledFront>
+);
+
+export default Tytuł;

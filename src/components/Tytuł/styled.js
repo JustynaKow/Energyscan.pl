c1818ee0e9ef0b50975e2ca9874ced42b84a3360
Justyna Kowalewska <justynajaglowska1@gmail.com>
2023-05-09 16:51:02 +0200

    changing style in Tytuł

--- src/components/Tytuł/styled.js
+++ src/components/Tytuł/styled.js
@@ -3,7 +3,7 @@ import tło from "../../images/background.png";
 
 export const StylledFront = styled.section`
     background: center / cover no-repeat url("${tło}");
-    height: 450px; 
+    height: 490px; 
     display: flex;
     flex-direction: column;
     justify-content: space-around;
@@ -15,7 +15,11 @@ export const Title = styled.h1`
     text-transform: uppercase;
     font-size: 3.5em;
     margin: 20px 50px;
-    width: 700px;
+    max-width: 600px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+        font-size: 1.5em;
+    }
 `;
 
 export const Subtitle = styled.h2`

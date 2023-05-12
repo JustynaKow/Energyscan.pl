e2b6e318941499eeba9e1e2f9c2e72d2892233c9
Justyna Kowalewska <justynajaglowska1@gmail.com>
2023-05-12 14:25:03 +0200

    changed padding in Przesłanki

--- src/components/Przesłanki/styled.js
+++ src/components/Przesłanki/styled.js
@@ -4,7 +4,7 @@ import tlo from "../../images/tlo.png";
 export const Section = styled.section`
     background: center / cover no-repeat url("${tlo}");
     color: ${({theme}) => theme.color.white};
-    padding-top: 100px;
+    padding-top: 105px;
 `;
 
 export const Title = styled.h2`

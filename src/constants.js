4cfc1a77588cb6d4632d90299da338bc8590fbb5
Justyna Kowalewska <justynajaglowska1@gmail.com>
2023-05-24 14:42:57 +0200

    adding adress to constants

--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,7 @@ const Contact = {
     PHONE: '+48530196930',
     EMAIL: 'biuro@energyscan.pl',
     FACEBOOK: 'https://www.facebook.com/profile.php?id=100092561449395',
+    ADRESS: `https://goo.gl/maps/4N47611gvZ6vr6icA`,
   };
 
   export default Contact;

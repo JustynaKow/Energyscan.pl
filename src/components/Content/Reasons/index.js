db9d100d1b8a78d233887935255f5b94c35c382d
Justyna Kowalewska <justynajaglowska1@gmail.com>
2023-05-26 13:01:05 +0200

    adding title

--- src/components/Content/Reasons/index.js
+++ src/components/Content/Reasons/index.js
@@ -1,16 +1,16 @@
-import { Article, Photo, Container, Wrapper, Box } from "./styled";
+import { Article, Photo, Container, Wrapper } from "./styled";
 import { Title, Section } from "../styled";
 import certyfikate from "../../../images/certyfikate.svg";
 import { reasons } from "./reasons";
 import ReasonsBox from "./ReasonsBox";
 
-const Reasons = () => (
+const Reasons = ({ title }) => (
     <Section id="reasons">
-        <Title> Kiedy potrzebujesz świadectwa charakterystyki energetycznej? </Title>
+        <Title> {title} </Title>
         <Container>
             <div>
                 <Article>
-                    Świadectwo charakterystyki energetycznej jest dokumentem, który informuje o efektywności energetycznej danego budynku. Wymóg posiadania certyfikatu dotyczy wszystkich budynków, niezależnie od roku budowy. Obowiązek zależy natomiast od podejmowanych działań.
+                    Świadectwo charakterystyki energetycznej jest dokumentem, który informuje o efektywności energetycznej danego budynku Obowiązek zależy od podejmowanych działań.
                 </Article>
                 <Photo src={certyfikate} alt="Certyfikat energetyczny" />
             </div>

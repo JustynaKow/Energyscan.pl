bf49afccb02b127f90d8e71eda609b82d1dd55c5
Justyna Kowalewska <justynajaglowska1@gmail.com>
2023-05-26 12:35:16 +0200

    add Clients

---
+++ src/components/Content/Clients/index.js
@@ -0,0 +1,44 @@
+import { Title, Section } from "../styled";
+import { Photo, Paragraph, Wrapper, Image, Article, Container, Case } from "./styled";
+import people from "../../../images/human.svg";
+import houses from "../../../images/houseNew.svg";
+
+const Clients = ({ title }) => {
+
+    const persons = [
+        "Właściciel",
+        "Zarządca",
+        "Osoby, którym przysługuje spółdzielcze własnościowe lub lokatorskie prawo do lokalu mieszkalnego",
+        "Każdy, kto będzie chciał sprzedać lub wynająć nieruchomość",
+    ];
+
+    return (
+        <Section>
+            <Title>
+                {title}
+            </Title>
+            <Container>
+                <Case>
+                    {persons.map((person) => (
+                        <Wrapper key={person}>
+                            <Photo src={people} alt="ikona osoby" />
+                            <Paragraph>
+                                {person}
+                            </Paragraph>
+                        </Wrapper>
+                    ))}
+                </Case>
+                <div>
+                    <Article>
+                        W przypadku braku świadectwa charakterystyki energetycznej podczas wynajmu lub
+                        sprzedaży nieruchomości istnieje ryzyko otrzymania grzywny o wysokości nawet do
+                        <strong>5 tysięcy złotych.</strong>
+                    </Article>
+                    <Image src={houses} alt="energooszczędny dom" />
+                </div>
+            </Container>
+        </Section>
+    );
+};
+
+export default Clients;
---
+++ src/components/Content/Clients/styled.js
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: grid;
+    grid-template-columns: auto 1fr;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+        grid-template-columns: 1fr;
+    }
+`;
+
+export const Case = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+`;
+
+export const Wrapper = styled.div`
+    margin: 20px;
+    display: flex;
+    align-items: center;
+`;
+
+export const Photo = styled.img`
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    box-shadow: 2px 1px 10px ${({ theme }) => theme.color.husk};
+`;
+
+export const Paragraph = styled.h5`
+    max-width: 400px;
+    font-size: 1em;
+    text-transform: uppercase;
+    margin: 10px 20px;
+`;
+
+export const Image = styled.img`
+    width: 500px;
+    border-radius: 40%;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+        width: 300px;
+    }
+`;
+
+export const Article = styled.p`
+    max-width: 500px;
+    line-height: 1.2;
+    margin: 30px 0;
+`;

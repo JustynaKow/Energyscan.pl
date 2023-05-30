1bf4af69c0d17f66020f58ddacaa642bf320e900
Justyna Kowalewska <justynajaglowska1@gmail.com>
2023-05-30 11:59:32 +0200

    adding Profit to Content

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,16 +1,18 @@
+import { Container } from "./styled";
 import Reasons from "./Reasons";
 import Price from "./Price";
 import Team from "./Team";
 import Services from "./Services";
 import Footer from "./Footer";
-import { Container } from "./styled";
 import Clients from "./Clients";
+import Profit from "./Profit";
 
 const Content = () => (
   <Container>
     <Reasons title="W jakich przypadkach potrzebujesz świadectwa charakterystyki energetycznej?"/>
     <Clients title="Kto powinien posiadać świadectwo charakterystyki energetycznej?" />
     <Price title="Ile to kosztuje?" />
+    <Profit title="O czym informuje świadectwo charakterystyki energetycznej?" />
     <Services />
     <Team />
     <Footer />

ac25366ed13f6c28db600b2f3654e1855482f1c1
Justyna Kowalewska <justynajaglowska1@gmail.com>
2023-06-02 13:10:09 +0200

    changes breakpoint in Media

--- src/components/Media/index.js
+++ src/components/Media/index.js
@@ -3,19 +3,27 @@ import massage from "../../images/massage.svg";
 import facebook from "../../images/facebook.svg";
 import Contact from "../../constants";
 import { Icon, Wrapper } from "./styled";
+import { useMediaQuery } from "react-responsive";
+import { theme } from "../../theme";
 
-const Media = () => (
-    <Wrapper>
-        <a href={`tel:${Contact.PHONE}`}>
-            <Icon src={phone} alt="Telefon" />
-        </a>
-        <a href={`mailto:${Contact.EMAIL}`}>
-            <Icon src={massage} alt="E-mail" />
-        </a>
-        <a href={`${Contact.FACEBOOK}`}>
-            <Icon src={facebook} alt="Facebook" />
-        </a>
-    </Wrapper>
-);
+const Media = () => {
+    const isMobile = useMediaQuery({ maxWidth: theme.breakpoint.mobileMax });
+
+    return (
+        !isMobile && (
+            <Wrapper>
+                <a href={`tel:${Contact.PHONE}`}>
+                    <Icon src={phone} alt="Telefon" />
+                </a>
+                <a href={`mailto:${Contact.EMAIL}`}>
+                    <Icon src={massage} alt="E-mail" />
+                </a>
+                <a href={`${Contact.FACEBOOK}`}>
+                    <Icon src={facebook} alt="Facebook" />
+                </a>
+            </Wrapper>
+        )
+    );
+};
 
 export default Media;

930f48f4fb91cd0fb2857f8a3cb805493cd9a96e
Justyna Kowalewska <justynajaglowska1@gmail.com>
2023-06-02 11:35:49 +0200

    changing padding in section

--- src/components/Content/styled.js
+++ src/components/Content/styled.js
@@ -14,7 +14,7 @@ export const Container = styled.div`
 `;
 
 export const Section = styled.section`
-    padding: 105px 100px 20px;
+    padding: 40px 100px 20px;
     width: 100%;
 
     @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {

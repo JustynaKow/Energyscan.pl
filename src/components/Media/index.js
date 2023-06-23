1b9b15b79d704b4c0d2279be5aa7476e8b4b2868
Justyna Kowalewska <justynajaglowska1@gmail.com>
2023-06-23 13:21:56 +0200

    changing img Icon

--- src/components/Media/index.js
+++ src/components/Media/index.js
@@ -1,6 +1,6 @@
-import phone from "../../images/phone.svg";
-import massage from "../../images/massage.svg";
-import facebook from "../../images/facebook.svg";
+import phone from "../../images/media-phone.svg";
+import massage from "../../images/media-massage.svg";
+import facebook from "../../images/media-facebook.svg";
 import Contact from "../../constants";
 import { Icon, Wrapper } from "./styled";
 import { useMediaQuery } from "react-responsive";
@@ -13,13 +13,13 @@ const Media = () => {
         !isMobile && (
             <Wrapper>
                 <a href={`tel:${Contact.PHONE}`}>
-                    <Icon src={phone} alt="Telefon" width="40px" height="40px" />
+                    <Icon src={phone} alt="Telefon" />
                 </a>
                 <a href={`mailto:${Contact.EMAIL}`}>
-                    <Icon src={massage} alt="E-mail" width="40px" height="40px"/>
+                    <Icon src={massage} alt="E-mail" />
                 </a>
                 <a href={`${Contact.FACEBOOK}`}>
-                    <Icon src={facebook} alt="Facebook" width="40px" height="40px"/>
+                    <Icon src={facebook} alt="Facebook" />
                 </a>
             </Wrapper>
         )
--- src/components/Media/styled.js
+++ src/components/Media/styled.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const Wrapper = styled.aside`
     position: fixed;
-    z-index: 3;
     padding: 0 20px;
     right: 0;
     bottom: 0;
@@ -13,11 +12,12 @@ export const Wrapper = styled.aside`
 `;
 
 export const Icon = styled.img`
+    width: 50px;
+    height: 50px;
     margin: 10px;
     transition: 0.5s linear;
 
     &:hover{
-        transform: scale(1.3);
-        filter: brightness(110%);
+        transform: scale(1.4);
     }
 `;

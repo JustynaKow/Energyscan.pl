eb235121ab91c54edfacb151ab15c502fde87d27
Justyna Kowalewska <justynajaglowska1@gmail.com>
2023-06-23 12:24:47 +0200

    adding new color to theme (grey, light)

--- src/theme.js
+++ src/theme.js
@@ -6,6 +6,8 @@ export const theme = {
         cararra: "#edede9",
         husk: "#fcc521",
         gamboge: "#E5A810",
+        grey: "#d9d9d9",
+        light: "#f0f0f0",
     },
     breakpoint: {
         mobileMax: 767,

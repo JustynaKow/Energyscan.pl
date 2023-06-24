bd17b31ac3fef613feab01c0a8aafefccd8dbbb8
Justyna Kowalewska <justynajaglowska1@gmail.com>
2023-06-24 12:53:40 +0200

    changing font size

--- src/components/Content/Information/styled.js
+++ src/components/Content/Information/styled.js
@@ -2,11 +2,12 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
     height: 200px;
-    font-size: 2em;
+    font-size: 1.7em;
     text-align: center;
     background-color: ${({ theme }) => theme.color.gamboge};
     color: ${({ theme }) => theme.color.white};
     display: flex;
     justify-content: center;
     align-items: center;
+    text-transform: uppercase;
 `;

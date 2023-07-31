15fc8ac1b53687735f1cf33a1aecfe6aa16ff044
Justyna Kowalewska <justynajaglowska1@gmail.com>
2023-07-31 13:49:11 +0200

    changing styles in Header

--- src/features/EnergyScan/Header/index.js
+++ src/features/EnergyScan/Header/index.js
@@ -1,18 +1,39 @@
-import { Wrapper, Heading, Subheading, Dot } from "./styled";
+import {
+  Element,
+  Wrapper,
+  Heading,
+  Container,
+  Subheading,
+  Dot,
+  Button,
+  Link,
+  Picture,
+} from "./styled";
+import Contact from "../../../constants";
+import background from "../../../images/mobile.webp";
 
 const Header = () => (
+  <Element>
     <Wrapper>
-        <Heading>
-            Świadectwa Charakterystyki Energetycznej
-        </Heading>
-        <Subheading>
-            Sejny
-            <Dot>·</Dot>
-            Suwałki
-            <Dot>·</Dot>
-            Augustów
-        </Subheading>
+        <Heading>Świadectwa Charakterystyki Energetycznej</Heading>
+        <Container>
+          <Subheading>Sejny</Subheading>
+          <Dot>·</Dot>
+          <Subheading>Suwałki</Subheading>
+          <Dot>·</Dot>
+          <Subheading>Augustów</Subheading>
+        </Container>
     </Wrapper>
+    <Picture src={background} alt="żarówka" />
+    <div>
+      <Button>
+        <Link href={`tel:${Contact.PHONE}`}>+48 530 196 930 </Link>
+      </Button>
+      <Button>
+        <Link href={`mailto:${Contact.EMAIL}`}>{Contact.EMAIL} </Link>
+      </Button>
+    </div>
+  </Element>
 );
 
 export default Header;
--- src/features/EnergyScan/Header/styled.js
+++ src/features/EnergyScan/Header/styled.js
@@ -1,43 +1,111 @@
 import styled from "styled-components";
-import background from "../../../images/background.jpg";
+import background from "../../../images/mobile.webp";
 
-export const Wrapper = styled.header`
-    background: center / cover no-repeat url("${background}");
-    background-attachment: fixed;
-    width: 100%;
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: center;
-    padding: 120px 10px 20px;
+export const Element = styled.header`
+  width: 100%;
+  height: 100vh;
+  padding: 100px 40px 0 40px;
+  background: url(${background});
+  background-position: bottom right;
+  background-repeat: no-repeat;
+  background-size: 800px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: flex-start;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    padding: 100px 0px 10px 0px;
+    justify-content: center;
+    background: none;
+
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.tabletMax}px) {
+    padding: 100px 0px 10px 0px;
+    background-position: 50% 60%;
+  }
+`;
+
+export const Picture = styled.img`
+    display: none;
 
     @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
-        background-attachment: scroll;
-    }
+        display: block;
+        max-width: 280px;
+        margin: 0 auto;
+  }
+`;
+
+export const Wrapper = styled.div`
+  font-family: "Montserrat", sans-serif;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-start;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    align-items: center;
+  }
 `;
 
 export const Heading = styled.h1`
-    font-size: 3rem;
-    text-transform: uppercase;
-    max-width: 900px;
+  font-size: 48px;
+  line-height: 1.3;
+  text-transform: uppercase;
+  max-width: 700px;
+  text-align: left;
+  margin-left: 20px;
 
-    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
-        font-size: 1.5rem;
-    }
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    font-size: 32px;
+    text-align: center;
+  }
+`;
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  margin-left: 10px;
 `;
 
 export const Subheading = styled.h2`
-    margin: 10px;
-    text-transform: uppercase;
-    display: flex;
-    flex-wrap: wrap;
-    align-items: center;
-    justify-content: center;
+  margin: 10px;
+  text-transform: uppercase;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    font-size: 16px;
+  }
 `;
 
 export const Dot = styled.span`
-    color: ${({ theme }) => theme.color.husk};
-    font-size: 3rem;
-    margin: 0 10px;
+  color: ${({ theme }) => theme.color.red};
+  font-size: 48px;
+  margin: 0 10px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    margin: 0;
+    font-size: 18px;
+  }
+`;
+
+export const Button = styled.button`
+  background-color: ${({ theme }) => theme.color.gamboge};
+  padding: 15px 10px;
+  border-radius: 10px;
+  border: none;
+  margin: 10px;
+  width: 230px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    margin: 10px;
+  }
+`;
+
+export const Link = styled.a`
+  text-transform: uppercase;
+  font-weight: bold;
+  text-decoration: none;
+  color: ${({ theme }) => theme.color.white};
 `;
